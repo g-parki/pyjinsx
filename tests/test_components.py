42538ff09bb6f89077180481880422fb82781274
g-parki <61096711+g-parki@users.noreply.github.com>
2022-01-13 11:10:40 -0800

    Add tests template_override, contentonly_tagless

--- tests/test_components.py
+++ tests/test_components.py
@@ -1,4 +1,5 @@
 from ..pyjinsx import *
+import pytest
 
 def test_image():
     output = '<img id="2" src="source" alt="alt">'
@@ -10,10 +11,25 @@ def test_link():
     link = Link("Text", "url", classes="my-class").render()
     assert(link == output)
 
+def test_div():
+    output = '<div style="my style">Text</div>'
+    div = Div("Text", style="my style").render()
+    assert(div == output)  
+
 def test_p():
     output = '<p style="my style">Text</p>'
     p = P("Text", style="my style").render()
-    assert( p == output)
+    assert(p == output)
+
+def test_template_override():
+    my_div = Div("world", template_override="Hello {{contents}}").render()
+    assert(my_div == "Hello world")
+
+def test_contentonly_tagless():
+    class Tagless(ContentOnly):
+        pass
+    with pytest.raises(ValueError):
+        _ = Tagless("content")
 
 def test_convenience_syntax():
     output = '<p id="id" class="my-class" style="my styles">Text</p>'
